13e3098b479f2689e9711a317f7e35bcacaa166a
Cristian Martinez <107958139+cmartineznvc@users.noreply.github.com>
2023-06-26 09:28:22 -0500

    Initial commit

---
+++ ExampleAppAPI.API/Data/ExampleAppAPIAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExampleAppAPI.API.Data.Models;
+
+namespace ExampleAppAPI.API.Data
+{
+    public class ExampleAppAPIAPIContext : DbContext
+    {
+        public ExampleAppAPIAPIContext (DbContextOptions<ExampleAppAPIAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExampleAppAPI.API.Data.Models.User> User { get; set; } = default!;
+    }
+}
---
+++ ExampleAppAPI.API/Data/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleAppAPI.API.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
